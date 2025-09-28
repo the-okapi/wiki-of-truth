fa8d7db9b42a6e8c5738b2e5f0eb188df2dd9fff
the-okapi <theo_kapi@outlook.com>
2025-09-27 18:10:41 -0700

    Adding stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,32 @@
-<script>
+<script lang="ts">
 	import { goto } from '$app/navigation';
 	import { resolve } from '$app/paths';
 	import { Button } from '$lib/components';
+	import { onMount } from 'svelte';
+
+	let loading = $state(false);
+
+	let articles: any[] = $state([
+		{
+			path: 'test',
+			title: 'This is a test!',
+			image: 'https://upload.wikimedia.org/wikipedia/en/8/80/Wikipedia-logo-v2.svg',
+			image_alt: 'Wikipedia Logo'
+		},
+		{
+			path: 'test2',
+			title: 'This is another test',
+			image:
+				'https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png',
+			image_alt: 'Phone'
+		}
+	]);
+	/*
+	onMount(async () => {
+		const response = await fetch('http://localhost:8000/featured');
+        articles = await response.json();
+        loading = false;
+	});*/
 </script>
 
 <svelte:head>
@@ -18,4 +43,23 @@
 			onclick={() => goto(resolve('/about'))}>About</Button
 		>
 	</p>
+	<div class="flex h-[60vh] items-center justify-center">
+		{#if loading}
+			<p class="font-bold">Loading...</p>
+		{:else}
+			{#each articles as article}
+				<button
+					class="mx-2 h-fit w-[12vw] cursor-pointer rounded-md border-2 p-[1vw] hover:shadow"
+					onclick={() => goto(resolve(`/wiki/${article.path}`))}
+				>
+					<img
+						src={article.image}
+						alt={article.image_alt}
+						class="m-auto mb-3 max-h-[13vw] max-w-[10vw] rounded"
+					/>
+					{article.title}
+				</button>
+			{/each}
+		{/if}
+	</div>
 </main>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,14 +4,11 @@
 
 <h1 class="mb-2 text-3xl font-bold">About</h1>
 
-<p>
-	The Wiki of Truth is an online wiki for storytelling. It is similar to Wikipedia as it has
-	articles on a lot of stuff, and it might not be true. (no offence to Wikipedia)
+<p class="m-auto mb-7 w-[75vw] text-center">
+	The Wiki of Truth is an online wiki for storytelling. It has articles on a lot of stuff, though
+	some of it might not be true. As the purpose of the Wiki of Truth is for enjoyment, most articles
+	on here might contain some misinformation, so you should take everything you read here with a
+	grain of salt.
 </p>
-<p>
-	As the purpose of the Wiki of Truth is for enjoyment, most articles on here might contain some
-	misinformation, so you should take everything you read here with a grain of salt.
-</p>
-<br />
 
 <img src="/about.webp" class="m-auto h-92" alt="About" />

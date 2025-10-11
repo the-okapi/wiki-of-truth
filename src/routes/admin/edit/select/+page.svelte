cf5d0768f1e48412fb7b957853c3443741bda2cc
the-okapi <theo_kapi@outlook.com>
2025-10-11 09:54:09 -0700

    Adding select article page

--- src/routes/admin/edit/select/+page.svelte
+++ src/routes/admin/edit/select/+page.svelte
@@ -1,3 +1,54 @@
+<script lang="ts">
+	import { articles } from '$lib';
+	import { onMount } from 'svelte';
+	import { Command, Label, Input, Button, T } from '$lib/components';
+	import { goto } from '$app/navigation';
+	import { resolve } from '$app/paths';
+
+	let adminUsername = $state('');
+
+	onMount(() => {
+		adminUsername = localStorage.getItem('admin_username') ?? '';
+	});
+
+	let open = $state(false);
+	let searchValue = $state('');
+
+	function goToPage(where: string) {
+		open = false;
+		goto(resolve(`/admin/edit/${where}`));
+	}
+
+	function openDialog(event: Event) {
+		event.preventDefault();
+		open = true;
+	}
+</script>
+
 <svelte:head>
-	<title>Select an Article to Edit</title>
+	<title>Select an Article to Edit - Wiki of Truth</title>
 </svelte:head>
+
+<Command.Dialog bind:open>
+	<Command.Input placeholder="Search for an article to edit" bind:value={searchValue} />
+	<Command.List>
+		<Command.Empty>No articles found.</Command.Empty>
+		{#each $articles as article (article.path)}
+			<Command.Item onclick={() => goToPage(article.path)}>{article.title} - Edit</Command.Item>
+		{/each}
+	</Command.List>
+</Command.Dialog>
+
+<div class="h-[85vh] w-screen p-8 align-middle">
+	<p class="text-xl font-bold"><T />{adminUsername}<T /></p>
+
+	<form onsubmit={openDialog} class="flex h-[60vh] items-center justify-center">
+		<div class="m-auto h-fit w-fit">
+			<Label for="searchInput">Search for an article to edit</Label>
+			<div class="mt-1 flex">
+				<Input id="searchInput" class="mr-1" bind:value={searchValue} />
+				<Button type="submit">Search</Button>
+			</div>
+		</div>
+	</form>
+</div>
